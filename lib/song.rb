f82f281e87427d4f6090c93fe8f364201caa7b0b
lidianvilazio <lidianvilazio@hotmail.com>
2018-02-26 09:18:38 -0500

    My Advanced Class Methods Lab

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,71 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    # self.all << self.new
+    song = self.new
+    self.all << song
+    song
+  end
+
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|i| i.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if name
+      if self.find_by_name(name)
+        self.find_by_name(name)
+      else
+        create_by_name(name)
+      end
+    else
+      create
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort! {|a,b| a.name <=> b.name}
+  end
+
+  def self.new_from_filename(name)
+    if name.include?("-")
+      name = name.split("-")
+      artist = name[0].split.join
+      name[-1] = name[-1].split(".") if name[-1].include?(".")
+      song = create_by_name(name[-1][0].split.join(" "))
+      song.artist_name = artist
+      song
+    end
+  end
+
+  def self.create_from_filename(name)
+    name = name.split("-")
+    artist = name[0].split.join
+    name[-1] = name[-1].split(".") if name[-1].include?(".")
+    song = create_by_name(name[-1][0].split.join(" "))
+    song.artist_name = artist
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+# binding.pry
 end
